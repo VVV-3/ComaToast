e21a6b835313a076d71dd3e3a6250f94d6466c1d
vijay vignesh <vijay.vignesh@research.iiit.ac.in>
2023-04-20 03:47:12 +0530

    1st submission

--- team_code.py
+++ team_code.py
@@ -14,14 +14,9 @@ import numpy as np, os, sys
 import mne
 from sklearn.impute import SimpleImputer
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn import svm
 import joblib
-
-from src.datasets import patientDataset, eegDataset
-from src.resnet import ResNet1d
-import torch
-from torch.utils.data import DataLoader
-
-import librosa
+import xgboost as xgb
 
 ################################################################################
 #
@@ -32,41 +27,78 @@ import librosa
 # Train your model.
 def train_challenge_model(data_folder, model_folder, verbose):
     # Find data files.
-    pattrainset = patientDataset(data_folder)
-    trainset = eegDataset(pattrainset, [i for i in range(len(pattrainset))])
-    trainloader = DataLoader(dataset=trainset, batch_size=16, shuffle=True, num_workers=20)
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if verbose >= 1:
+        print('Finding the Challenge data...')
+
+    patient_ids = find_data_folders(data_folder)
+    num_patients = len(patient_ids)
+
+    if num_patients==0:
+        raise FileNotFoundError('No data was provided.')
 
     # Create a folder for the model if it does not already exist.
     os.makedirs(model_folder, exist_ok=True)
 
-    # Train the models.
-    train_config = { 'num_epochs':20, 'learning_rate':1e-4 }
-    arch_config  = {
-                'n_input_channels':18,
-                'signal_length':75,
-                'net_filter_size':[ 18],
-                'net_signal_length':[ 75],
-                'kernel_size': 3,
-                'n_classes':2,
-                'dropout_rate':0.15
-               }
+    # Extract the features and labels.
+    if verbose >= 1:
+        print('Extracting features and labels from the Challenge data...')
 
-    model = ResNet1d(input_dim=(arch_config['n_input_channels'], arch_config['signal_length']), 
-                     blocks_dim=list(zip(arch_config['net_filter_size'], arch_config['net_signal_length'])),
-                     kernel_size=arch_config['kernel_size'],
-                     dropout_rate=arch_config['dropout_rate'])
-    model = model.to(device)
-    criterion = torch.nn.CrossEntropyLoss(reduction='sum')
-    optimizer = torch.optim.AdamW(model.parameters(), lr=train_config['learning_rate'], weight_decay=1e-7)
+    features = list()
+    outcomes = list()
+    cpcs = list()
+
+    for i in range(num_patients):
+        if verbose >= 2:
+            print('    {}/{}...'.format(i+1, num_patients))
+
+        # Load data.
+        patient_id = patient_ids[i]
+        patient_metadata, recording_metadata, recording_data = load_challenge_data(data_folder, patient_id)
+
+        # Extract features.
+        current_features = get_features(patient_metadata, recording_metadata, recording_data)
+        features.append(current_features)
+
+        # Extract labels.
+        current_outcome = get_outcome(patient_metadata)
+        outcomes.append(current_outcome)
+        current_cpc = get_cpc(patient_metadata)
+        cpcs.append(current_cpc)
+
+    features = np.vstack(features)
+    outcomes = np.vstack(outcomes)
+    cpcs = np.vstack(cpcs)
 
     # Train the models.
-    train_losses = []
-    for epoch in range(train_config['num_epochs']):
-        train_losses.append(train_one_epoch(model, device, optimizer, criterion, trainloader, epoch))
+    if verbose >= 1:
+        print('Training the Challenge models on the Challenge data...')
+
+    # Define parameters for random forest classifier and regressor.
+    n_estimators   = 144  # Number of trees in the forest.
+    max_leaf_nodes = 146  # Maximum number of leaf nodes in each tree.
+    random_state   = 1717  # Random state; set for reproducibility.
+
+    # Impute any missing features; use the mean value by default.
+    imputer = SimpleImputer().fit(features)
+
+    # Train the models.
+    features = imputer.transform(features)
+    
+    outcome_model = xgb.XGBClassifier(
+                                        n_estimators=150, tree_method='hist',
+                                        reg_lambda=0.85, max_leaves=100,
+                                        subsample=0.4,
+                                        max_depth=500, grow_policy = 'depthwise',
+                                        random_state=random_state
+                                    ).fit(features, outcomes.ravel())
+#     outcome_model = RandomForestClassifier(
+#         n_estimators=n_estimators, max_leaf_nodes=max_leaf_nodes, random_state=random_state).fit(features, outcomes.ravel())
+    
+    cpc_model = RandomForestRegressor(
+        n_estimators=n_estimators, max_leaf_nodes=max_leaf_nodes, random_state=random_state).fit(features, cpcs.ravel())
 
     # Save the models.
-    save_challenge_model(model_folder, [], model, [])
+    save_challenge_model(model_folder, imputer, outcome_model, cpc_model)
 
     if verbose >= 1:
         print('Done.')
@@ -80,21 +112,29 @@ def load_challenge_models(model_folder, verbose):
 # Run your trained models. This function is *required*. You should edit this function to add your code, but do *not* change the
 # arguments of this function.
 def run_challenge_models(models, data_folder, patient_id, verbose):
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = models['outcome_model'].to(device)
-    model.eval()
+    imputer = models['imputer']
+    outcome_model = models['outcome_model']
+    cpc_model = models['cpc_model']
 
     # Load data.
-    inputs = load_test_data(data_folder, patient_id)
+    patient_metadata, recording_metadata, recording_data = load_challenge_data(data_folder, patient_id)
 
-    # Run Model
-    inputs  = inputs.type(torch.FloatTensor).to(device)
-    outputs = model(inputs).detach().cpu().numpy().mean(axis=0)
-    outcome = np.argmax(outputs)
-    outcome_probability = outputs[1]
-    cpc = int(5*outcome_probability) + 1
-    
-#     print(inputs.shape, outputs.shape, outcome_probability, outcome)
+    # Extract features.
+    features = get_features(patient_metadata, recording_metadata, recording_data)
+    features = features.reshape(1, -1)
+
+    # Impute missing data.
+    features = imputer.transform(features)
+
+    # Apply models to features.
+    outcome = outcome_model.predict(features)
+    outcome_probability = outcome_model.predict_proba(features)[0,1]
+#     print(outcome_probability)
+    cpc = cpc_model.predict(features)[0]
+
+    # Ensure that the CPC score is between (or equal to) 1 and 5.
+    cpc = np.clip(cpc, 1, 5)
+
     return outcome, outcome_probability, cpc
 
 ################################################################################
@@ -108,96 +148,6 @@ def save_challenge_model(model_folder, imputer, outcome_model, cpc_model):
     d = {'imputer': imputer, 'outcome_model': outcome_model, 'cpc_model': cpc_model}
     filename = os.path.join(model_folder, 'models.sav')
     joblib.dump(d, filename, protocol=0)
-    
-    
-def train_one_epoch(model, device, optimizer, criterion ,train_loader, epoch):
-    model.train()
-    train_loss = []
-    for i, (inputs, labels) in enumerate(train_loader): 
-            
-        inputs = inputs.type(torch.FloatTensor).to(device)
-        labels = labels.type(torch.FloatTensor).reshape((labels.shape[0],2)).to(device)
-        
-        outputs = model(inputs)
-        loss    = criterion(outputs, labels)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        
-        train_loss.append(loss.cpu().detach().numpy())
-        print("Epoch {}: {}/{} Loss: {}".format(epoch, i, len(train_loader), loss.item()), end='\r')
-    return np.mean(train_loss)
-
-def getMetadata(data_folder, patient_id):
-    # Define file location.
-    patient_metadata_file = os.path.join(data_folder, patient_id, patient_id + '.txt')
-    recording_metadata_file = os.path.join(data_folder, patient_id, patient_id + '.tsv')
-
-    # Load non-recording data.
-    patient_metadata = load_text_file(patient_metadata_file)
-    recording_metadata = load_text_file(recording_metadata_file)
-        
-    return patient_metadata, recording_metadata
-
-def load_test_data(data_folder, patient_id):
-    inps = []
-    
-    no_of_segments = 1
-    self_channels = ['Fp1-F7', 'F7-T3', 'T3-T5', 'T5-O1', 'Fp2-F8', 'F8-T4', 
-                         'T4-T6', 'T6-O2', 'Fp1-F3', 'F3-C3', 'C3-P3', 'P3-O1', 
-                         'Fp2-F4', 'F4-C4', 'C4-P4', 'P4-O2', 'Fz-Cz', 'Cz-Pz']
-    self_l_cutoff = 0.1
-    self_h_cutoff = 10
-    self_order = 3
-        
-    self_final_sr = 2*self_h_cutoff
-        
-    self_st_time = 0
-    self_end_time = 300
-    self_hop_time = 10
-    self_segment_length = 20
-    
-    def bandpassFilter(signal, sampling_rate):
-        nyq = 0.5 * sampling_rate
-        normal_l_cutoff = self_l_cutoff / nyq
-        normal_h_cutoff = self_h_cutoff / nyq
-
-        # get the filter coefficients
-        b, a = sp.signal.butter( self_order, [normal_l_cutoff, normal_h_cutoff], btype='bp', analog=False)
-        filtered_signal = sp.signal.filtfilt(b, a, signal)
-        return filtered_signal
-        
-    def addRecording(recording_data, sr):
-        filtered_data  = bandpassFilter(recording_data, sr)
-        resampled_data = librosa.resample(y=filtered_data, orig_sr=sr, target_sr=self_final_sr)
-        mod_data, _ = mne.time_frequency.psd_array_welch(recording_data, sfreq=sr,  
-                                                      fmin=0.5,  fmax=30.0, verbose=False)
-        
-#         for st_time in range(self_st_time, self_end_time, self_hop_time):
-#             end_time = st_time+self_segment_length
-#             if end_time > self_end_time:
-#                 continue
-                
-#             st_mkr, end_mkr = st_time*self_final_sr, end_time*self_final_sr
-#             recording_segment = resampled_data[:, st_mkr:end_mkr]
-     
-        inps.append(mod_data)
-    
-    patient_metadata, recording_metadata = getMetadata(data_folder, patient_id)
-    recording_ids = list(get_recording_ids(recording_metadata))
-    ct=0
-    for recording_id in reversed(recording_ids):
-        if recording_id != 'nan':
-            recording_location = os.path.join(data_folder, patient_id, recording_id)
-            recording_data, sampling_frequency, channels = load_recording(recording_location)
-            recording_data = reorder_recording_channels(recording_data, channels, self_channels)
-            addRecording(recording_data, sampling_frequency)
-            ct+=1
-            
-        if ct >= no_of_segments:
-            break
-            
-    return torch.Tensor(np.array(inps))
 
 # Extract features from the data.
 def get_features(patient_metadata, recording_metadata, recording_data):
@@ -225,7 +175,7 @@ def get_features(patient_metadata, recording_metadata, recording_data):
         other  = 1
 
     # Combine the patient features.
-    patient_features = np.array([age, female, male, other, rosc, ohca, vfib, ttm])
+    patient_features = np.array([age]) #[age, female, male, other, rosc, ohca, vfib, ttm]
 
     # Extract features from the recording data and metadata.
     channels = ['Fp1-F7', 'F7-T3', 'T3-T5', 'T5-O1', 'Fp2-F8', 'F8-T4', 'T4-T6', 'T6-O2', 'Fp1-F3',
@@ -277,7 +227,8 @@ def get_features(patient_metadata, recording_metadata, recording_data):
         delta_psd_mean = theta_psd_mean = alpha_psd_mean = beta_psd_mean = float('nan') * np.ones(num_channels)
         quality_score = float('nan')
 
-    recording_features = np.hstack((signal_mean, signal_std, delta_psd_mean, theta_psd_mean, alpha_psd_mean, beta_psd_mean, quality_score))
+#     recording_features = np.hstack((signal_mean, signal_std, delta_psd_mean, theta_psd_mean, alpha_psd_mean, beta_psd_mean, quality_score))
+    recording_features = np.hstack((signal_mean, signal_std,delta_psd_mean, theta_psd_mean, alpha_psd_mean, beta_psd_mean))
 
     # Combine the features from the patient metadata and the recording data and metadata.
     features = np.hstack((patient_features, recording_features))
